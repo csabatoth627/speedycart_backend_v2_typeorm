72a735330b5f0b3081dea71e94a411f38f9d4700
Csaba <csabi627@gmail.com>
2024-02-22 20:08:34 +0100

    feat: refactor (data-source.ts)

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,21 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import * as dotenv from "dotenv";
+dotenv.config();
 import { User } from "./entity/User"
+
+const PORT = parseInt(process.env.DB_PORT);
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+const DATABASE = process.env.DB_NAME
 
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
+    port: PORT,
+    username: USERNAME,
+    password: PASSWORD,
+    database: DATABASE,
     synchronize: true,
     logging: false,
     entities: [User],
