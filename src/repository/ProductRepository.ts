bd587ac61d5af02bb2339353fb75823441ca141f
Csaba <csabi627@gmail.com>
2024-02-26 17:38:37 +0100

    feat: create saveSampleProduct (productRepository.ts)

--- src/repository/ProductRepository.ts
+++ src/repository/ProductRepository.ts
@@ -1,20 +1,44 @@
 import { AppDataSource } from "../config/data-source";
 import { Product } from "../entity/Product";
 import { Repository } from "typeorm";
+import { User } from "../entity/User";
+
+const userRepository: Repository<User> = AppDataSource.getRepository(User);
 
 const productRepository: Repository<Product> =
   AppDataSource.getRepository(Product);
 
-const getAllProducts = () => {
-  return productRepository.find();
+const getAllProducts = async (): Promise<Product[]> => {
+  return await productRepository.find();
 };
 
-const findProductById = (id) => {
-  return productRepository.findOne({
+const findProductById = async (id: string): Promise<Product | undefined> => {
+  return await productRepository.findOne({
     where: {
       _id: id,
     },
   });
+};
+
+const saveSampleProduct = async (userId: string) => {
+  const user: User = await userRepository.findOne({
+    where: {
+      _id: userId,
+    },
+  });
+
+  const product: Product = new Product();
+  product.name = 'Sample name'
+  product.price = 0
+  product.user = user
+  product.image = '/images/sample.jpg'
+  product.brand = 'Sample brand'
+  product.category = 'Sample Category'
+  product.countInStock = 0,
+  product.numReviews = 0,
+  product.description = 'Sample description'
+
+  return await productRepository.save(product)
 };
 
 export { getAllProducts, findProductById };
