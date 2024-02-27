eeda874745b61a25e0eb4852d9f1af715afd2f7b
Csaba <csabi627@gmail.com>
2024-02-27 13:33:42 +0100

    feat: create authenticate function(userRepository.ts)

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -3,3 +3,25 @@ import { Repository } from "typeorm";
 import { User } from "../entity/User";
 
 const userRepository: Repository<User> = AppDataSource.getRepository(User);
+
+const authenticate = async (
+  userEmail: string,
+  userPassword: string
+): Promise<User> => {
+  const user: User = await userRepository.findOne({
+    select: {
+      _id: true,
+      name: true,
+      email: true,
+      isAdmin: true,
+    },
+    where: {
+      email: userEmail,
+    },
+  });
+
+  return user;
+};
+
+
+export {authenticate}
